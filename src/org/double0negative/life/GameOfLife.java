511c0f9bf97fc1a09d8e14b4e7446e7f5226bbba
Double0negative <Double0negative@mc-sg.org>
2013-10-10 03:46:04 -0400

    Game of life

---
+++ src/org/double0negative/life/GameOfLife.java
@@ -0,0 +1,9 @@
+package org.double0negative.life;
+
+public class GameOfLife {
+	
+	public static void main(String args[]){
+		//new LifeGame(100,70,100,-1);
+		new LifeGamePureRandom(100,70,10,-1).start();  //x,y,size,seed. Increas x/y and decrease size for more density
+	}
+}

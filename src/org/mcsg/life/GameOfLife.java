b5aa1d8adf60f90203fadb1f90fde84f82da65e5
Double0negative <Double0negative@mc-sg.org>
2013-10-10 03:45:56 -0400

    Game of life

--- src/org/mcsg/life/GameOfLife.java
+++
@@ -1,9 +0,0 @@
-package org.mcsg.life;
-
-public class GameOfLife {
-	
-	public static void main(String args[]){
-		//new LifeGame(100,70,100,-1);
-		new LifeGamePureRandom(100,70,10,-1).start();  //x,y,size,seed. Increas x/y and decrease size for more density
-	}
-}
